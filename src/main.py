76ec11c06ebc78c940d67b02851ba19ce1dcad98
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2024-07-14 14:10:49 +0300

    Почти конец

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def probability_of_purchase(x, y):
-    return 1 - math.exp(-y / x)
-
-# Пример: среднее количество дней между покупками x = 10, спустя y = 5 дней
-x = 4
-y = 28
-print(probability_of_purchase(x, y))
